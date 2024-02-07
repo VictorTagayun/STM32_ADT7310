ceee02f4c709c7ed8ac524c9a87dd4658530ceb9
victortagayun <vic.tagayun@yahoo.com>
2024-02-07 15:43:29 +0800

    Add files via upload

---
+++ NUCLEO-G474RE_ADT7310_V1/Core/Inc/VT_ADT7310.h
@@ -0,0 +1,22 @@
+/*
+ * ADT7310.h
+ *
+ *  Created on: Feb 5, 2024
+ *      Author: https://victortagayun.github.io
+ */
+
+#ifndef INC_VT_ADT7310_H_
+#define INC_VT_ADT7310_H_
+
+#define Status   	0x00 //  8-bit
+#define Config 	 	0x01 //  8-bit
+#define Temp_val 	0x02 // 16-bit
+#define ID 		 	0x03 //  8-bit
+#define TCRIT_setpt 0x04 // 16-bit
+#define THYST_setpt	0x05 //  8-bit
+#define THIGH_setpt	0x06 // 16-bit
+#define TLOW_setpt 	0x07 // 16-bit
+
+
+
+#endif /* INC_VT_ADT7310_H_ */
---
+++ NUCLEO-G474RE_ADT7310_V1/Core/Src/VT_ADT7310.c
@@ -0,0 +1,8 @@
+/*
+ * VT_ADT7310.c
+ *
+ *  Created on: Feb 6, 2024
+ *      Author: testing
+ */
+
+
